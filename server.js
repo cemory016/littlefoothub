45de082e53104238dfcaec3c794fe4551331e1ed
Carmen <cemory016@gmail.com>
2018-03-21 18:11:56 -0400

    create react app

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
-mongoose.connect(process.env.MONGODB_URI); //mongodb://localhost/idea-board
+mongoose.connect(process.env.MONGODB_URI); 
 
 const connection = mongoose.connection;
 connection.on('connected', () => {
   console.log('Mongoose Connected Successfully')
 })
+app.use(logger('dev'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // If the connection throws an error
 connection.on('error', (err) => {
@@ -20,7 +23,9 @@ app.get('/', (req,res) => {
   res.send('Hello world!')
 })
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log("Magic happening on port " + PORT);
 })
+
+module.exports = app
